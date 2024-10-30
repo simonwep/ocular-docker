9b02aad119acb408bb06eee2349a31a0f949ad1c
Simon Reinisch <simon@reinisch.io>
2024-10-30 19:25:57 +0100

    feat: add setup script for even easier installation
    bump nginx version

--- README.md
+++ README.md
@@ -13,9 +13,19 @@
 
 <br/>
 
-> [!NOTE]  
-> For release notes, check out the [latest release](https://github.com/simonwep/ocular/releases/latest) in the [ocular](https://github.com/simonwep/ocular) repository.
-> This repo is just for production releases :)
+## Quick start
+
+To download the latest release and start it via docker-compose, run:
+
+```sh
+bash <(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/simonwep/ocular-docker/refs/heads/main/setup.sh)
+```
+
+> [!NOTE]
+> This will perform the same steps as described in the [manual setup](#manual-setup-and-migration) section and ask you questions if needed.
+> You can always [take a look at the script](setup.sh) before running it, or perform [these steps manually](#manual-setup-and-migration).
+
+## Manual setup and migration
 
 ### First time setup
 
@@ -34,11 +44,11 @@ To migrate to a newer version, follow these steps:
 
 1. Backup the `./data` folder. The folder contains all the user-data.
 2. Download the [latest release](https://github.com/simonwep/ocular-docker/releases/latest) and extract it.
-3. Copy the `.env.example` to `.env`, adjust the values if needed. You don't need to run `./gen-passwords.sh` again.
+3. Copy the `.env.example` to `.env`, adjust the values if needed. **You don't need to run `./gen-passwords.sh` again.**
 4. Copy your old `./data` folder to the new location.
 5. Run `docker compose up -d`.
 
-### Admin controls
+## Admin controls
 
 You can use [genesis's CLI](https://github.com/simonwep/genesis?tab=readme-ov-file#cli) to manage users.
 For example, to change a user's password:
@@ -52,9 +62,44 @@ For help run:
 docker run --rm -v "$(pwd)/data:/app/.data" --env-file .env ghcr.io/simonwep/genesis:latest help
 ```
 
-### FAQ
+## FAQ
 
-> Help! I can't log in to the app over the network!
+### Where can I find the release notes?
+
+For release notes, check out the [latest release](https://github.com/simonwep/ocular/releases/latest) in the [ocular](https://github.com/simonwep/ocular) repository.
+This repo is just for production releases :)
+
+### Help! I can't log in to the app over the network!
 
 If you don't use https, make sure to set `GENESIS_JWT_COOKIE_ALLOW_HTTP` to `true` in your `.env` file.
 Otherwise, run it behind a reverse proxy like [nginx](https://www.nginx.com/) and get a free certificate from [letsencrypt](https://letsencrypt.org/).
+
+Make sure to restart the app after changing the `.env` file via `docker compose restart`.
+
+### What kind of config do I need if I want to run it behind an nginx reverse proxy?
+
+Here's an example of a basic nginx config (v1.25+):
+
+```nginx
+server {
+    listen 443 quic reuseport;
+    listen 443 ssl;
+
+    server_name ocular.example.com;
+    add_header Alt-Svc 'h3=":443"; ma=86400';
+
+    location / {
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Server $host;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+        proxy_read_timeout 300s;
+        proxy_pass http://127.0.0.1:3030$request_uri;
+    }
+
+    # Specify the path to your certificate and key, or use letsencrypt
+    #ssl_certificate
+    #ssl_certificate_key 
+}
+```
---
+++ setup.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+set -eu
+
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Function to get the latest release version from a GitHub repository
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+# Check if Docker is installed
+if ! command_exists docker; then
+  echo "Docker is not installed. Please install Docker and try again."
+  exit 1
+fi
+
+# Check if Docker Compose is installed
+if ! command_exists docker-compose; then
+  echo "Docker Compose is not available. Please install the latest version of Docker and try again."
+  exit 1
+fi
+
+# Get the latest release version
+repository="simonwep/ocular-docker"
+latest_version=$(get_latest_release "$repository")
+latest_version_clean="${latest_version#v}"
+echo "The latest release version of $repository is $latest_version"
+
+# Check if the folder already exists and if it should be removed first
+if [ -d "ocular-docker-$latest_version_clean" ]; then
+  echo "The folder ocular-docker-$latest_version_clean already exists."
+  read -r -p " ? Do you want to remove it and continue? This will remove all data! (yes/no): " confirm
+  if [ "$confirm" == "yes" ]; then
+    echo " ✔ Removing existing folder..."
+    rm -rf "ocular-docker-$latest_version_clean"
+  else
+    echo " ⅹ Aborting setup."
+    exit 1
+  fi
+fi
+
+# Download the latest release source code
+echo "Downloading the latest release source code..."
+curl -sL "https://github.com/$repository/archive/refs/tags/$latest_version.tar.gz" -o ocular-docker.tar.gz
+
+# Extract the release
+echo " → Extracting the release..."
+tar -xzf ocular-docker.tar.gz
+rm -rf ocular-docker.tar.gz
+cd "ocular-docker-$latest_version_clean" || { echo "Failed to enter the ocular-docker directory"; exit 1; }
+
+# Copy .env.example to .env
+echo "Setting up environment variables..."
+cp .env.example .env
+
+# Ask the user if the service should be available via HTTP
+read -r -p " ? Should the service be available via HTTP? (yes/no): " allow_http
+if [ "$allow_http" == "yes" ]; then
+  sed -i.bak 's/GENESIS_JWT_COOKIE_ALLOW_HTTP=false/GENESIS_JWT_COOKIE_ALLOW_HTTP=true/' .env
+  echo " ✔ The service will be available via HTTP."
+else
+  echo " ⅹ The service will not be available via HTTP."
+fi
+
+# Generate secrets and an initial admin user
+echo "Generating secrets and an initial admin user..."
+./gen-passwords.sh > /dev/null
+
+# Start Docker Compose services
+echo "Starting Docker Compose services..."
+docker compose up -d
+
+# Extract generated credentials
+GENESIS_CREATE_USERS=$(grep 'GENESIS_CREATE_USERS' .env | cut -d '=' -f2)
+ADMIN_USER=$(echo "$GENESIS_CREATE_USERS" | cut -d '!' -f1)
+ADMIN_PASSWORD=$(echo "$GENESIS_CREATE_USERS" | cut -d ':' -f2)
+
+# Texts for the info box
+texts=(
+  "Setup complete!"
+  "Ocular should be accessible under http://localhost:3030 in your browser :)"
+  ""
+  "You can log in as admin using the following credentials:"
+  " → Username: $ADMIN_USER"
+  " → Password: $ADMIN_PASSWORD"
+  ""
+  "What's next?"
+  " → cd into the 'ocular-docker-$latest_version_clean' directory and run 'docker compose down' to stop the services."
+  " → your data is located under the 'ocular-docker-$latest_version_clean/data' directory."
+)
+
+# Print info box
+max_length=$(printf "%s\n" "${texts[@]}" | awk '{ if ( length > L ) { L=length } } END { print L }')
+box_width=$((max_length + 4))
+
+printf '┌%*s┐\n' "$box_width" '' | tr ' ' '─'
+for text in "${texts[@]}"; do
+  printf "│ %-${max_length}s   │\n" "$text"
+done
+printf '└%*s┘\n' "$box_width" '' | tr ' ' '─'
